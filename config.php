c1936596d4626faacce2b16888b1471de3340a1d
Nikolarobase <75938500+Nikolarobase@users.noreply.github.com>
2021-01-17 13:19:42 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-
-    <?php
+ <?php
 $config=$CONFIG=array("root_path"=>"/BD_DRUIDE/")
-
-     ?>
-  </body>
-</html>
+ ?>
