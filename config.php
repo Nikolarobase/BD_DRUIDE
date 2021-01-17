140d1a10828249d20e1e717cbc4be7c0ff2aebaa
Nikolarobase <75938500+Nikolarobase@users.noreply.github.com>
2021-01-17 13:12:20 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+$config=$CONFIG=array("root_path"=>"/BD_DRUIDE/")
+
+     ?>
+  </body>
+</html>
